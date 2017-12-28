4e06a740b7275b717a067ebbe3c2c3bf86245e6d
davincif <LDaVinciF@gmail.com>
2017-12-28 10:49:58 -0300

    everything setup

--- davincif/urls.py
+++ davincif/urls.py
@@ -16,7 +16,9 @@
 
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^', include("home.urls")),
 ]
